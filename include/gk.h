41a60fa6ad950b0da5e9e07d0ecd89e095bcdd61
Recep Aslantas <info@recp.me>
2016-10-05 10:47:25 +0300

    model and complex model data structure
    * simple render function

--- include/gk.h
+++ include/gk.h
@@ -12,5 +12,69 @@
 #include "gk-shader.h"
 #include "gk-program.h"
 #include <cglm.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define GK_DRAW_ELEMENTS (1 << 0)
+#define GK_DRAW_ARRAYS   (1 << 1)
+#define GK_COMPLEX       (1 << 2)
+
+struct GkModelBase;
+struct GkModel;
+struct GkModelInstance;
+
+typedef void (*gkOnDraw)(struct GkModelBase * model,
+                         struct GkModelInstance * instance,
+                         bool finished);
+
+typedef void (*gkOnClick)(struct GkModelInstance * instance,
+                          float x,
+                          float y);
+
+typedef struct GkModelBase {
+  void    *data;
+  gkOnDraw onDraw;
+  int32_t  flags;
+} GkModelBase;
+
+typedef struct GkModel {
+  GkModelBase base;
+  GLuint  *vbo;
+  GLuint  *tex;
+  GLuint   vao;
+  uint32_t vboCount;
+  GLsizei  count;
+  GLenum   mode;
+} GkModel;
+
+typedef struct GkComplexModel {
+  GkModelBase base;
+  GLuint     *vao;
+  GLuint     *vbo;
+  GLuint     *tex;
+  GLsizei    *count;
+  GLenum     *modes;
+  GLuint      vaoCount;
+  uint32_t    vboCount;
+} GkComplexModel;
+
+typedef struct GkModelInstance {
+  GkModelBase *model;
+  void        *data;
+  gkOnDraw     onDraw;
+  gkOnClick    onClick;
+  vec4        *matrix;
+  GLint        matrixLoc;
+  int32_t      flags;
+  struct GkModelInstance *next;
+} GkModelInstance;
+
+GkModelInstance *
+gkMakeInstance(GkModelBase *model,
+               mat4 matrix,
+               GLint matrixLoc);
+
+void
+gkRender(GkModelInstance * modelInstance);
 
 #endif /* gk_h */
--- src/gk.c
+++ src/gk.c
@@ -7,4 +7,98 @@
 
 #include "../include/gk.h"
 
+static
+inline
+void
+gkRenderSimple(GkModelInstance * modelInstance);
 
+static
+inline
+void
+gkRenderComplex(GkModelInstance * modelInstance);
+
+/*---------------------------------------------------------------------------*/
+
+GkModelInstance *
+gkMakeInstance(GkModelBase *model,
+               mat4 matrix,
+               GLint matrixLoc) {
+  GkModelInstance *instance;
+
+  instance            = calloc(sizeof(*instance), 1);
+  instance->matrix    = matrix;
+  instance->matrixLoc = matrixLoc;
+  instance->model     = model;
+  return instance;
+}
+
+void
+gkRender(GkModelInstance * modelInstance) {
+  GkModelBase *model;
+
+  model = modelInstance->model;
+
+  /* pre events */
+  if (modelInstance->onDraw)
+    modelInstance->onDraw(model, modelInstance, false);
+
+  if (model->onDraw)
+    model->onDraw(model, modelInstance, false);
+
+  /* render */
+  if (model->flags & GK_COMPLEX)
+    gkRenderComplex(modelInstance);
+  else
+    gkRenderSimple(modelInstance);
+
+  /* post events */
+  if (modelInstance->onDraw)
+    modelInstance->onDraw(model, modelInstance, true);
+
+  if (model->onDraw)
+    model->onDraw(model, modelInstance, true);
+}
+
+static
+inline
+void
+gkRenderSimple(GkModelInstance * modelInstance) {
+  GkModel *model;
+  model = (GkModel *)modelInstance->model;
+
+  glBindVertexArray(model->vao);
+
+  if (modelInstance->flags & GK_DRAW_ELEMENTS)
+    glDrawElements(model->mode, 0, model->mode, NULL);
+  else if (modelInstance->flags & GK_DRAW_ARRAYS)
+    glDrawArrays(model->mode, 0, model->count);
+
+  glBindVertexArray(0);
+}
+
+static
+inline
+void
+gkRenderComplex(GkModelInstance * modelInstance) {
+  GkComplexModel *model;
+  uint32_t index;
+
+  model = (GkComplexModel *)modelInstance->model;
+
+  for (index = 0; index < model->vaoCount; index++) {
+    glBindVertexArray(model->vao[index]);
+
+    if (model->base.flags & GK_DRAW_ELEMENTS) {
+      glDrawElements(GL_TRIANGLES,
+                     model->count[index],
+                     GL_UNSIGNED_INT, /* TODO: ? */
+                     NULL);
+    } else if (modelInstance->flags & GK_DRAW_ARRAYS) {
+      glDrawArrays(model->modes[index],
+                   0,
+                   model->count[index]);
+    }
+
+    glBindVertexArray(0);
+  }
+}
