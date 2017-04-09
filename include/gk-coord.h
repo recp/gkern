d3a2e2acd8883cc1ce1cf9231367029744aef289
Recep Aslantas <info@recp.me>
2017-04-09 15:36:29 +0300

    move origin of vector/position util

---
+++ include/gk-coord.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef gk_coord_h
+#define gk_coord_h
+
+#include "gk-common.h"
+#include <cglm.h>
+
+void
+gk_moveOrigin(vec3 position,
+              vec3 orig,
+              vec3 out);
+
+#endif /* gk_coord_h */
---
+++ src/coord/gk_coord_orig.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#include "../../include/gk.h"
+#include "../../include/gk-coord.h"
+
+void
+gk_moveOrigin(vec3 position,
+              vec3 orig,
+              vec3 out) {
+  glm_vec_sub(orig, position, out);
+}
