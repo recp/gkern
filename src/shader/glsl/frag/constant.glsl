b8ae4cc85affdec9029831849996404b53960cb4
Recep Aslantas <info@recp.me>
2017-10-08 12:15:26 +0300

    fix constant material shader

--- src/shader/glsl/frag/constant.glsl
+++ src/shader/glsl/frag/constant.glsl
@@ -8,38 +8,19 @@
 GK_STRINGIFY(
 
 void main() {
-  vec4  lightc;
-  vec3  L, H;
-  float a;
-
-  switch (lightType) {
-    case SpotLight:
-      a = spot(L);
-      break;
-    case PointLight:
-      a = point(L);
-      break;
-    case DirectionalLight:
-      a = directional(L);
-      break;
-    default:
-      discard;
-      return;
-  }
-
-  lightc = light.color * a;
-
   fragColor =
-\n#ifdef AMBIENT_TEX\n
-  texture(tex2D[mat.ambient], vTexCoord[mat.ambient])
-\n#elif defined(AMBIENT_COLOR)\n
-  mat.ambient
+\n#ifdef EMISSION_TEX\n
+  texture(tex2D[mat.emission], vTexCoord[mat.emission])
+\n#elif defined(EMISSION_COLOR)\n
+  mat.emission
+\n#else\n
+  vec4(0, 0, 0, 1)
 \n#endif\n
 
-\n#ifdef EMISSION_TEX\n
-  + texture(tex2D[mat.emission], vTexCoord[mat.emission])
-\n#elif defined(EMISSION_COLOR)\n
-  + mat.emission
+\n#ifdef AMBIENT_TEX\n
+  + texture(tex2D[mat.ambient], vTexCoord[mat.ambient])
+\n#elif defined(AMBIENT_COLOR)\n
+  + mat.ambient
 \n#endif\n
 ;
 
