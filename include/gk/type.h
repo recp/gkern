bfdd2d3a7b4395ff7012ce0ed3f3a1486b9d5790
Recep Aslantas <info@recp.me>
2017-11-13 10:47:56 +0300

    define types enum to wrap opengl enums
    because we want to hide opengl enums from end user

---
+++ include/gk/type.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef type_h
+#define type_h
+
+#include "common.h"
+
+typedef enum GkType {
+  GKT_UNKNOWN = 0,
+  GKT_INT32   = 1,
+  GKT_UINT32  = 2,
+  GKT_INT16   = 3,
+  GKT_UINT16  = 4,
+  GKT_INT8    = 5,
+  GKT_UINT8   = 6,
+  GKT_FLOAT   = 7,
+  GKT_DOUBLE  = 8
+} GkType;
+
+#endif /* type_h */
