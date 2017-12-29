264df36048102b592abf2f3039338f3399f94189
Recep Aslantas <info@recp.me>
2017-12-29 17:22:22 +0300

    stage missing scene_impl

---
+++ src/types/impl_scene.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef impl_scene_h
+#define impl_scene_h
+
+#include "../../include/gk/scene.h"
+#include "../../include/gk/node.h"
+#include "impl_node.h"
+
+#include <ds/forward-list.h>
+
+typedef struct GkSceneImpl {
+  GkScene            pub;
+  struct FListItem  *transpPrims;
+  struct GkLight    *forLight;
+  void              *shadows;
+  struct GkPass     *overridePass;     /* override all passes    */
+  struct GkMaterial *overrideMaterial; /* override all materials */
+  FList             *transfCacheSlots;
+  GkRenderPathFn     rp;
+  GkRenderPathType   rpath;
+  GkRect             vrect;
+  int32_t            internalFormat;
+  float              backingScale;
+} GkSceneImpl;
+
+#endif /* impl_scene_h */
