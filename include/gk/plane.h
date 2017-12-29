16908b05f7117f02ccaf4f33c1092a09f3a5b7dd
Recep Aslantas <info@recp.me>
2017-12-29 23:47:50 +0300

    define plane enum to access planes without remembering index

--- include/gk/plane.h
+++ include/gk/plane.h
@@ -10,6 +10,15 @@
 
 #include <cglm/cglm.h>
 
+typedef enum GkPlaneIndex {
+  GK_PLANE_LEFT   = 0,
+  GK_PLANE_RIGHT  = 1,
+  GK_PLANE_BOTTOM = 2,
+  GK_PLANE_TOP    = 3,
+  GK_PLANE_NEAR   = 4,
+  GK_PLANE_FAR    = 5
+} GkPlaneIndex;
+
 typedef vec4 GkPlane;
 
 #endif /* plane_h */
