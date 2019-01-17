a1d82c83396e8ebbe6ca30262fcdee8007d34ce2
Recep Aslantas <info@recp.me>
2019-01-17 11:07:31 +0300

    update types

--- include/gk/type.h
+++ include/gk/type.h
@@ -11,17 +11,19 @@
 #include "common.h"
 
 typedef enum GkType {
-  GKT_UNKNOWN = 0,
-  GKT_INT32   = 1,
-  GKT_UINT32  = 2,
-  GKT_INT16   = 3,
-  GKT_UINT16  = 4,
-  GKT_INT8    = 5,
-  GKT_UINT8   = 6,
-  GKT_FLOAT   = 7,
-  GKT_DOUBLE  = 8,
-  GKT_FLOAT3  = 9,
-  GKT_FLOAT4  = 10
+  GKT_UNKNOWN  = 0,
+  GKT_INT32    = 1,
+  GKT_UINT32   = 2,
+  GKT_INT16    = 3,
+  GKT_UINT16   = 4,
+  GKT_INT8     = 5,
+  GKT_UINT8    = 6,
+  GKT_FLOAT    = 7,
+  GKT_DOUBLE   = 8,
+  GKT_FLOAT3   = 9,
+  GKT_FLOAT4   = 10,
+  GKT_FLOAT3x3 = 11,
+  GKT_FLOAT4x4 = 12
 } GkType;
 
 #endif /* type_h */
